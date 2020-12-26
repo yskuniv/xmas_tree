c8340531a1c94b24d00016e052524a7fb632ddea
ysk_univ <ysk.univ.1007@gmail.com>
2020-12-27 01:14:40 +0900

    add xmas_tree_workload.c

---
+++ xmas_tree_workload.c
@@ -0,0 +1,83 @@
+#include <busy.h>
+#include <pthread.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <utils.h>
+
+#define MAX_NUM_THREADS 16
+#define TREE_RATE_MAX 256U
+
+struct tree_thread_args
+{
+    int th_num;
+    int tree_width;
+};
+
+void do_own_part_of_tree(const int tree_rate)
+{
+    for (;;)
+    {
+        do_busy((65536U / TREE_RATE_MAX) * tree_rate);
+        do_busy_in_kernel((65536U / TREE_RATE_MAX) * (TREE_RATE_MAX - tree_rate));
+    }
+}
+
+void *tree_thread(void *arg)
+{
+    const struct tree_thread_args *const th_args = (struct tree_thread_args *)arg;
+    const int th_num = th_args->th_num;
+    const int tree_width = th_args->tree_width;
+
+    bind_on_cpu(th_num);
+
+    const int c = (tree_width + 2) / 2;
+    const int d = abs(c - (th_num + 1));
+    const int tree_rate = (c - d) * TREE_RATE_MAX / c;
+
+    do_own_part_of_tree(tree_rate);
+
+    return NULL;
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc <= 1)
+    {
+        fputs("Error: tree width not specified\n", stderr);
+        exit(1);
+    }
+    else if (argc > 2)
+    {
+        fputs("Error: too many arguments\n", stderr);
+        exit(1);
+    }
+
+    const int tree_width = atoi(argv[1]);
+    const int num_threads = tree_width;
+
+    if (tree_width <= 0 || tree_width > MAX_NUM_THREADS)
+    {
+        fputs("Error: illegal tree width specified\n", stderr);
+        exit(1);
+    }
+
+    pthread_t threads[MAX_NUM_THREADS];
+    struct tree_thread_args th_args[MAX_NUM_THREADS];
+
+    for (int i = 0; i < num_threads; i++)
+    {
+        th_args[i].th_num = i;
+        th_args[i].tree_width = tree_width;
+
+        pthread_create(&threads[i], NULL, tree_thread, &th_args[i]);
+    }
+
+    for (int i = 0; i < num_threads; i++)
+    {
+        pthread_join(threads[i], NULL);
+    }
+
+    return 0;
+}
