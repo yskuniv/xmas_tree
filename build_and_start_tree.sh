f7e4d40ee12e852f712ba10ef51701008c45b919
ysk_univ <ysk.univ.1007@gmail.com>
2022-09-07 22:02:10 +0900

    Revert dangerous commit "[build_and_start_tree.sh] fix"
    This reverts commit 74a219344ac1e9a87c638ade618a3944a684349c.

--- build_and_start_tree.sh
+++ build_and_start_tree.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-while ! vagrant up --provision; do :; done && \
+vagrant up --provision && \
 vagrant ssh-config > tmp/ssh-config && \
 ssh -F tmp/ssh-config -o RequestTTY=yes default /vagrant/scripts/show_xmas_tree.sh
