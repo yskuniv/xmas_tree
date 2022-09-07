74a219344ac1e9a87c638ade618a3944a684349c
ysk_univ <ysk.univ.1007@gmail.com>
2022-09-07 21:27:04 +0900

    [build_and_start_tree.sh] fix

--- build_and_start_tree.sh
+++ build_and_start_tree.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-vagrant up --provision && \
+while ! vagrant up --provision; do :; done && \
 vagrant ssh-config > tmp/ssh-config && \
 ssh -F tmp/ssh-config -o RequestTTY=yes default /vagrant/scripts/show_xmas_tree.sh
